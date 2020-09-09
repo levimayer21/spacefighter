0d7d63f110a051b4c087b849602e6bb6f6b9361d
Mayer Levente <leventemayer1@hotmail.hu>
2020-09-09 20:51:00 +0200

    Parallax tries 2020909

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,10 @@
 $(document).ready(function()
 {
     $('.sidenav').sidenav();
+    $('.parallax').parallax();
 }
 );
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
