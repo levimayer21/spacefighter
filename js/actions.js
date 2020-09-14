6893912ebab67d7a6431fb84152573056fa70309
Mayer Levente <leventemayer1@hotmail.hu>
2020-09-14 20:31:06 +0200

    Scroll kinda done, phone and safari nope

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,3 @@
-var speedtosize = document.getElementsByTagName("nav")[0].clientWidth / 210;
-
 $(document).ready(function()
 {
     $('.sidenav').sidenav();
@@ -7,6 +5,49 @@ $(document).ready(function()
 }
 );
 
-document.documentElement.style.setProperty("--navheight", document.getElementById("spacenav").clientHeight + "px");
-document.documentElement.style.setProperty("--marginHeight", (document.getElementsByTagName("body")[0].clientHeight / 45) + "px");
+var navHeight = document.getElementById("spacenav").clientHeight + "px";
+var marHeight = (document.getElementsByTagName("body")[0].clientHeight / 45) + "px";
+var navWidth = document.getElementsByTagName("nav")[0].clientWidth;
+
+if(navWidth < 601)
+{
+    var speedtosize = 3;
+}
+else if (navWidth < 993)
+{
+    var speedtosize = 4.5;
+}
+else
+{
+    var speedtosize = navWidth / 250;
+}
+
+document.documentElement.style.setProperty("--navheight", navHeight);
+document.documentElement.style.setProperty("--marginHeight", marHeight);
 document.documentElement.style.setProperty("--shipspeed", speedtosize + "s");
+document.body.style.setProperty("--navheight", navHeight);
+document.body.style.setProperty("--marginHeight", marHeight);
+document.body.style.setProperty("--shipspeed", speedtosize + "s");
+
+var topBtn = document.getElementById("toTopButton");
+
+
+//Scroll funkció, érzékeli ha a toTop gombnak meg kell-e jelennie
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() 
+{
+    if (document.documentElement.scrollTop > 20 || document.body.scrollTop > 20)
+    {
+        topBtn.style.display = "block";
+    }
+    else
+    {
+        topBtn.style.display = "none";
+    }
+}
+
+function toTop()
+{
+    window.scroll({top: 0, left: 0, behavior: 'smooth'});
+}
