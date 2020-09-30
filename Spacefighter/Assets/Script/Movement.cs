3fc8de320d783c41f833787a1e61cf1bd27786bf
Mayer Levente <leventemayer1@hotmail.hu>
2020-09-30 18:08:56 +0200

    Hope this works

---
+++ Spacefighter/Assets/Script/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed = 1.0f;
+    Rigidbody2D playerrig;
+
+    private void Start()
+    {
+        playerrig = gameObject.GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    public void ToLeft()
+    {
+        playerrig.AddForce(new Vector2(-speed, 0), ForceMode2D.Force);
+    }
+}
