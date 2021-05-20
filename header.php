f2978f287958f5ae8556d99cba2f1f541a2852e5
Mayer Levente <leventemayer1@hotmail.hu>
2021-05-20 09:57:44 +0200

    2021.05.20 fixing cookies

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <?php
 ini_set("session.cookie_httponly", TRUE);
 ini_set("session.cookie_secure", TRUE);
+ini_set("session.samesite", "Lax");
+ini_set("session.use_strict_mode", TRUE);
 
 ?>
 <!DOCTYPE html>
